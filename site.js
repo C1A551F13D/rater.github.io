2fab8d4118f5cc745befc0bc87b0c0d8f147ffff
C1A551F13D <shane.walters@live.com>
2023-10-23 16:59:59 -0600

    Clean up results

--- site.js
+++ site.js
@@ -17,18 +17,22 @@ const rateMovie = async (movieTitleElem, commentElem) => {
 		};
 		const response = await fetch('https://unwelcomeimage-rater.hf.space/api/predict', options);
 		const json = await response.json(); //extract JSON from the http response
-		table = document.getElementById('resultsTable');
+
+		table = document.querySelector('#resultsTable > tbody');
+
 		var json2 = JSON.parse(json.data).sort(function (a, b) {
 			return a[0].localeCompare(b[0]);
 		});
+
+		table.innerHTML = '';
 		json2.forEach(movie => {
 			var row = table.insertRow();
 
 			var title = row.insertCell(0);
 			var rating = row.insertCell(1);
 
-			title.innerHTML = movie[0];
-			rating.innerHTML = movie[1];
+			title.innerText = movie[0];
+			rating.innerText = movie[1];
 		});
 	}
 
