d8b9b3125ee0020d33126624c95c634a305924dd
C1A551F13D <shane.walters@live.com>
2023-10-23 14:00:37 -0600

    Fix API

--- site.js
+++ site.js
@@ -5,15 +5,17 @@ const rateMovie = async (movieTitleElem, commentElem) => {
 	if (movieTitle && comment) {
 		const response = await fetch('https://unwelcomeimage-rater.hf.space/api/predict', {
 			method: 'POST',
-			body: [
-				movieTitle,
-				comment
-			],
+			body: {
+				"data": [
+					movieTitle,
+					comment
+				]
+			},
 			headers: {
 				'Content-Type': 'application/json'
 			}
 		});
-		const json = await response.json(); //extract JSON from the http response
+		const json = await response.json().data; //extract JSON from the http response
 		table = document.getElementById('resultsTable');
 		var json2 = json.sort(function (a, b) {
 			return a[0].localeCompare(b[0]);
